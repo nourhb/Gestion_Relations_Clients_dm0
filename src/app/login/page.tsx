a0deb1dc75a8e7c7f299b63de34e03b9cd872b00
nourelhoudabouajila <61210180+nourhb@users.noreply.github.com>
2025-07-22 14:49:55 +0100

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,21 @@
 
-import LoginForm from "@/components/auth/LoginForm";
+"use client";
 
-export default function LoginPage() {
+import LoginForm from "@/components/auth/LoginForm";
+import { Suspense } from "react";
+
+function LoginPageContent() {
   return (
     <div className="container mx-auto px-4 py-12 flex justify-center items-center min-h-[calc(100vh-8rem)]">
       <LoginForm isAdminLogin={true} />
     </div>
+  );
+}
+
+export default function LoginPage() {
+  return (
+    <Suspense>
+      <LoginPageContent />
+    </Suspense>
   );
 }
